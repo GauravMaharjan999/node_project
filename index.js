6e1607ce8e37787117c6bbdc410cbe41bca683bb
Gaurav Maharjan <rijanmaharjan999@gmail.com>
2021-10-03 10:15:46 +0545

    new file

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const http = require('http')
-const url = require('url')
+const http = require('http');
+const url = require('url');
+
+let content = "";
+let statusCode = "";
+
+
+
 
 const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('<h1>Hello World</h1>');
+    response.end();
+});
 
-})
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running at port: ${port}`);
+});
